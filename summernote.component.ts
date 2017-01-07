69b214b7cb68c82273be65630f37a5f32d71c3c7
carles <carlescs@gmail.com>
2017-01-07 21:07:28 +0100

    Started adding ngModel.

--- summernote.component.ts
+++ summernote.component.ts
@@ -1,32 +1,75 @@
 /// <reference path="summernote.d.ts" />
 
-
-import {Component,
+import {
+    Component,
     OnInit,
     OnDestroy,
     ElementRef,
-    Input
+    Input,
+    forwardRef
 } from '@angular/core';
 
+import {
+    NG_VALUE_ACCESSOR
+    , ControlValueAccessor
+} from '@angular/forms';
+
+const SUMMERNOTE_VALUE_ACCESSOR = {
+  provide: NG_VALUE_ACCESSOR,
+  useExisting: forwardRef(() => SummernoteComponent),
+  multi: true
+};
+
 @Component({
-    selector:'summernote',
-    template: '<div class="summernote"></div>'
+    selector: 'summernote',
+    template: '<div class="summernote"></div>',
+    providers:[SUMMERNOTE_VALUE_ACCESSOR]
 })
-export class SummernoteComponent implements OnInit,OnDestroy{
+export class SummernoteComponent implements OnInit, OnDestroy, ControlValueAccessor {
     @Input() options: SummernoteOptions;
 
-    constructor(private element:ElementRef) {
+    private onTouched = () => { };
+    private onChange: (value: string) => void = () => { };
+
+    constructor(private element: ElementRef) {
 
     }
+    private _value: string;
 
-    ngOnInit(){
-        if(this.options==null){
-            this.options={};
+    set value(value: string) {
+        this._value = value;
+    }
+
+    ngOnInit() {
+        if (this.options == null) {
+            this.options = {};
         }
+        this.options.callbacks={
+            onChange:(contents, $editable)=>{
+                this.onChange(contents);
+            },
+            onTouched:()=>{
+                this.onTouched();
+            }
+        };
         $(this.element.nativeElement).find('.summernote').summernote(this.options);
     }
 
-    ngOnDestroy(){
+    ngOnDestroy() {
         $(this.element.nativeElement).find('.summernote').summernote('destroy');
+    }
+
+    writeValue(code: string) {
+        this.value = code;
+
+        jQuery(this.element.nativeElement).summernote('code', this.value);
+    }
+
+    registerOnChange(fn: any) {
+        this.onChange = fn;
+    }
+
+    registerOnTouched(fn: any) {
+        this.onTouched = fn;
     }
 }
