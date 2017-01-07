716385cd7c716271e6a386a8fad47f0cffb2b4e6
carles <carlescs@gmail.com>
2017-01-07 20:33:07 +0100

    Add options.

--- summernote.component.ts
+++ summernote.component.ts
@@ -1,19 +1,29 @@
 /// <reference path="summernote.d.ts" />
 
 
-import {Component,OnInit,OnDestroy,ElementRef} from '@angular/core';
+import {Component,
+    OnInit,
+    OnDestroy,
+    ElementRef,
+    Input
+} from '@angular/core';
 
 @Component({
     selector:'summernote',
     template: '<div class="summernote"></div>'
 })
 export class SummernoteComponent implements OnInit,OnDestroy{
+    @Input() options: SummernoteOptions;
+
     constructor(private element:ElementRef) {
 
     }
 
     ngOnInit(){
-        $(this.element.nativeElement).find('.summernote').summernote();
+        if(this.options==null){
+            this.options={};
+        }
+        $(this.element.nativeElement).find('.summernote').summernote(this.options);
     }
 
     ngOnDestroy(){
